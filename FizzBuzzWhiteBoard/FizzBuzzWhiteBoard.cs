2795577a15d2fa56840593aa4ee4cc92d5262204
RichardRodi <RichardmRodi@gmail.com>
2024-03-15 15:23:53 -0400

    Attempted the white board challenge. not sure if its right

---
+++ FizzBuzzWhiteBoard/FizzBuzzWhiteBoard.cs
@@ -0,0 +1,16 @@
+
+public string FizzBuzz(int Number)
+{
+if (number % 3 == 0 && % 5 == 0)
+{
+    Console.WriteLine("Fizzbuzz")
+}
+else if (number % 3 == 0 )
+{ 
+    Console.Writeline("Fizz")
+}
+else if( number % 5 == 0)
+{
+    Console.WriteLine(Buzz)
+}
+}
